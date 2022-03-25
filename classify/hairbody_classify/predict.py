a755f962c5fbae004fac92f8fdb04e470bc6a69a
mone-sj <mone.sj21@gmail.com>
2022-03-25 16:05:32 +0900

    model_pt생성

--- classify/hairbody_classify/predict.py
+++ classify/hairbody_classify/predict.py
@@ -8,7 +8,7 @@ from ..gpu_set import gpuNumber_set
 
 p = argparse.ArgumentParser()
 
-p.add_argument('--model_load_path', default='./classify/hairbody_classify/', type=str)
+p.add_argument('--model_load_path', default='./classify/model_pt/', type=str)
 p.add_argument('--model_file', default='best_model_hairbody.pt', type=str)
 p.add_argument('--prefix', default='/', type=str)
 args = p.parse_args()
