bab5fa649025c14ef79ac4bdf13e6969e4e881f3
yuna <my10078@naver.com>
2022-04-11 14:53:24 +0900

    mess

--- classify/gpu_set.py
+++ classify/gpu_set.py
@@ -6,6 +6,7 @@ def gpuNumber_set():
         #os.environ["CUDA_DEVICE_ORDER"]="PCI_BUS_ID"
         os.environ["CUDA_VISIBLE_DEVICES"]= "0,1"
         device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+        print(device)
     except:
         device=torch.device('cpu')
     return device
